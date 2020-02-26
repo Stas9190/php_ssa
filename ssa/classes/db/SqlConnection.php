7140f0d91f22ef362f43d7655019847aded7b332
Шульга Станислав Алексеевич <ShulgaSA@energomera.ru>
2020-02-26 16:06:19 +0300

    Переписал модуль SqlConnection для совместитмости с db_connection

--- ssa/classes/db/SqlConnection.php
+++ ssa/classes/db/SqlConnection.php
@@ -1,182 +1,165 @@
 <?php
+
+/**
+ * @author ShulgaSA
+ * @version 1.0.0
+ * @package SqlConnection
+ */
+
+namespace SqlConnection;
+
 //Подключение к бд
-class SqlConnection 
+class SqlConnection
 {
 	//Параметры подключения к бд
 	var $DATABASES;
 	//Указываем какой профиль настроек подтягиваем из settings.php DEFAULT - по умолчанию
 	var $DATABASE_PROFILE_NAME;
-		
-	function __construct ()
+
+	function __construct()
 	{
 		$a = func_get_args();
 		$i = func_num_args();
-		switch($i)
-		{
+		switch ($i) {
 			case 1:
-				$this -> DATABASES = $a[0];
-				$this -> DATABASE_PROFILE_NAME = "DEFAULT";
+				$this->DATABASES = $a[0];
+				$this->DATABASE_PROFILE_NAME = "DEFAULT";
 				break;
 			case 2:
-				$this -> DATABASES = $a[0];
-				$this -> DATABASE_PROFILE_NAME = $a[1];
+				$this->DATABASES = $a[0];
+				$this->DATABASE_PROFILE_NAME = $a[1];
 				break;
 		}
 	}
-	
-	//Подключаемся к Microsoft SQL Server
-	//Открываем соединение с бд, используя параметры подключение из settings.DATABASES
-	function OpenConnection()
+
+	/**
+	 * Подключаемся к Microsoft SQL Server
+	 * Открываем соединение с бд, используя параметры подключение из settings.DATABASES
+	 * 
+	 * @return object
+	 */
+	function Connect()
 	{
-		$Database = $this -> DATABASES[$this -> DATABASE_PROFILE_NAME]["NAME"];
-		$CharacterSet = $this -> DATABASES[$this -> DATABASE_PROFILE_NAME]["CHARSET"];
-		$serverName = $this -> DATABASES[$this -> DATABASE_PROFILE_NAME]["HOST"];
-		$UID = $this -> DATABASES[$this -> DATABASE_PROFILE_NAME]["USER"];
-		$PWD = $this -> DATABASES[$this -> DATABASE_PROFILE_NAME]["PASSWORD"];
-		
-		$connectionInfo = array("Database"=>$Database, "CharacterSet"=>$CharacterSet, "UID"=>$UID, "PWD"=>$PWD);
+		$Database = $this->DATABASES[$this->DATABASE_PROFILE_NAME]["NAME"];
+		$CharacterSet = $this->DATABASES[$this->DATABASE_PROFILE_NAME]["CHARSET"];
+		$serverName = $this->DATABASES[$this->DATABASE_PROFILE_NAME]["HOST"];
+		$UID = $this->DATABASES[$this->DATABASE_PROFILE_NAME]["USER"];
+		$PWD = $this->DATABASES[$this->DATABASE_PROFILE_NAME]["PASSWORD"];
+
+		$connectionInfo = array("Database" => $Database, "CharacterSet" => $CharacterSet, "UID" => $UID, "PWD" => $PWD);
 		$conn = sqlsrv_connect($serverName, $connectionInfo);
-		
+
 		return $conn;
-	}
-	
-	function isExist()
-	{
-		if ($this -> OpenConnection())
-			return true;
-		else
-			return false;
 	}
 }
 
 //Получение данных из бд
-class SqlCommand
+class Command
 {
 	var $conn;
 	var $sql;
 	var $type;
 	var $params;
-	
+
 	function __construct()
 	{
 		$a = func_get_args();
 		$i = func_num_args();
-		switch($i)
-		{
+		switch ($i) {
 			case 2:
-				$this -> conn = $a[0];
-				$this -> sql = $a[1];
-				$this -> type = null;
-				$this -> params = null;
+				$this->conn = $a[0];
+				$this->sql = $a[1];
+				$this->type = null;
+				$this->params = null;
 				break;
 			case 4:
-				$this -> conn = $a[0];
-				$this -> sql = $a[1];
-				$this -> type = $a[2];
+				$this->conn = $a[0];
+				$this->sql = $a[1];
+				$this->type = $a[2];
 				//массив
-				$this -> params = $a[3];
+				$this->params = $a[3];
 				break;
 		}
 	}
-	
-	function requestExecute()
-	{		
+
+	function Execute()
+	{
 		//Получаем переменные класса
-		$conn = $this -> conn;
-		$sql = $this -> sql;
+		$conn = $this->conn;
+		$sql = $this->sql;
 		$type = null;
 		$params = null;
-		if ($this -> type != null)
-			$type = $this -> type;
-		if ($this -> params != null)
-			$params = $this -> params;
-		
+		if ($this->type != null)
+			$type = $this->type;
+		if ($this->params != null)
+			$params = $this->params;
+
 		// Для вставки нельзя передавать массив
 		//Если получили массив
 		//Не запрос на выборку
-		if ($type != null && $type == "non_query")
-		{
+		if ($type != null && $type == "non_query") {
 			//на вход поступают con, sql, non_query и массив params
-			if (!is_array($sql))
-			{
+			if (!is_array($sql)) {
 				$stmt = sqlsrv_query($conn, $sql, $params);
-				if ($stmt === false)
-				{
-					die( print_r( sqlsrv_errors(), true));
+				if ($stmt === false) {
+					die(print_r(sqlsrv_errors(), true));
 					$result["status"] = 0;
 					$result["message"] = sqlsrv_errors();
-				}
-				else
-				{
+				} else {
 					$result["status"] = 1;
 					$result["message"] = "Запрос успешно выполнен";
 				}
-				
+
 				return $result;
-			}
-			else
-			{
+			} else {
 				$result["status"] = 0;
 				$result["message"] = "Получен неверный перечень параметров";
 				return $result;
 			}
 		}
 		//Запрос на выборку данных
-		else
-		{
-			if (is_array($sql))
-			{
+		else {
+			if (is_array($sql)) {
 				$err = false;
 				$i = 0;
-				foreach ($sql as $key => $val)
-				{
+				foreach ($sql as $key => $val) {
 					$stmt = sqlsrv_query($conn, $val);
-					if ($stmt === false)
-					{
+					if ($stmt === false) {
 						$err = true;
 						$result["message"][$key] = sqlsrv_errors();
 					}
 					$res = array();
 					$i = 0;
-			
-					while ($data = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
-					{
+
+					while ($data = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 						$res[$i] = $data;
 						$i++;
-					}				
-					
+					}
+
 					$result["data"][$key] = $res;
 					$i++;
 				}
-				if ($err)
-				{
+				if ($err) {
 					$result["status"] = 0;
-				}
-				else
-				{
+				} else {
 					$result["status"] = 1;
 					$result["message"] = "Запрос успешно выполнен";
 				}
 			}
 			//Если одиночный запрос
-			else
-			{
+			else {
 				$stmt = sqlsrv_query($conn, $sql);
-				if ($stmt === false)
-				{
+				if ($stmt === false) {
 					$result["status"] = 0;
 					$result["message"] = sqlsrv_errors();
-				}
-				else 
-				{
+				} else {
 					$result["status"] = 1;
 					$result["message"] = "Запрос успешно выполнен";
 				}
 				//Получаем данные из бд
 				$res = array();
 				$i = 0;
-				while ($data = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
-				{
+				while ($data = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
 					$res[$i] = $data;
 					$i++;
 				}
@@ -187,4 +170,3 @@ class SqlCommand
 		}
 	}
 }
-?>
